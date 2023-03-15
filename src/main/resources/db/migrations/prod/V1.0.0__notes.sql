e44e77011aea0976be5a854d111b0d50a74a9fad
Marco Caimi <mcaimi@redhat.com>
2023-03-15 08:41:51 +0100

    Move application code to a dedicated repo

--- src/main/resources/db/migrations/prod/V1.0.0__notes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE notes
-(
-  id   BIGINT,
-  name VARCHAR(100),
-  contents VARCHAR(10000)
-);
-CREATE SEQUENCE notes_id_sequential;
--- src/main/resources/db/migrations/test/V1.0.0__notes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE notes
-(
-  id   INT,
-  name VARCHAR(100),
-  contents VARCHAR(10000)
-);
-CREATE SEQUENCE notes_id_sequential;
